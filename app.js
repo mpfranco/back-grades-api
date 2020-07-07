f89b009bf2b301079d6a2aa11a479eeea8efc5da
michel.franco <michel.platini.franco@gmail.com>
2020-07-07 19:51:46 -0300

    rm yarn.lock

--- app.js
+++ app.js
@@ -30,7 +30,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    //origin: 'http://localhost:3000',
+    origin: 'https://fathomless-chamber-80317.herokuapp.com/',
   })
 );
 
